dbcb05e5983a9b3b0af97726deca11644df49c5d
Dialmee <maggie.fabre02@gmail.com>
2026-04-09 09:12:21 +0200

    [UI]MainMenuTest

---
+++ space-jam-99/mainMenu2D.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene_Game_01.tscn")
+
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
