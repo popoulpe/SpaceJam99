f785a4d6089a5121266b65723794c5669a24db65
Dialmee <maggie.fabre02@gmail.com>
2026-04-11 11:42:55 +0200

    [UI]autosizeIsOk

--- space-jam-99/Scripts/UI/mainMenu2D.gd
+++ space-jam-99/Scripts/UI/mainMenu2D.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,13 +11,13 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_start_button_pressed() -> void:
+func _on_start_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene_Game_01.tscn")
 
 
-func _on_options_button_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_quit_button_pressed() -> void:
+func _on_quit_texture_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_options_texture_button_pressed() -> void:
+	pass # Replace with function body.
