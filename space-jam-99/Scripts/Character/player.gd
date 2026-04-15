7897fffe4471d9f67be74b94baacacb7cf5b2b1c
popoulpe <54855619+popoulpe@users.noreply.github.com>
2026-04-15 10:30:23 +0200

    Squashed commit of the following:
    commit 5de576d48598fc71db7383e46fff3581eb4ea6a8
    Author: popoulpe <54855619+popoulpe@users.noreply.github.com>
    Date:   Tue Apr 14 16:02:26 2026 +0200
    
        time implemented
    
    commit ce2991ff8f278b5f819e9f997dc31c25065b42a4
    Author: popoulpe <54855619+popoulpe@users.noreply.github.com>
    Date:   Tue Apr 14 13:03:01 2026 +0200
    
        obstacles and invulnerability after hurt
    
    commit b4ca0ea9f6614dd06a2152c677dda34d7a36642b
    Author: popoulpe <54855619+popoulpe@users.noreply.github.com>
    Date:   Mon Apr 13 11:52:23 2026 +0200
    
        player fix
    
    commit 950b608b95f9e59b675cc9a8857adb7f9c675c39
    Author: popoulpe <54855619+popoulpe@users.noreply.github.com>
    Date:   Sun Apr 12 22:44:55 2026 +0200
    
        ? ca c'est fait tout seul

--- space-jam-99/Scripts/Character/player.gd
+++ space-jam-99/Scripts/Character/player.gd
@@ -1,1 +1,21 @@
 extends Node
+
+@onready var character_body_3d = $CharacterBody3D
+@export var hud:hud
+@export var time :float = 60
+@export var timePenality :float = 5
+@export var invulnerabilityDuration:float=2 
+
+func _process(delta):
+	if time>0:
+		time-=delta
+		hud.time_update(time, false)
+	elif time>-10:
+		time = -10
+		print('YOU LOSE !')
+
+func get_hit():
+	if time <=0:
+		return
+	time -= timePenality
+	hud.time_update(time, true)
--- space-jam-99/Scripts/Character/to_delete_speed.gd
+++ space-jam-99/Scripts/Character/to_delete_speed.gd
@@ -1,7 +1,23 @@
 extends Control
+class_name hud
 
 @onready var character_body_3d = $".."
-@onready var label = $Label
+@onready var temps = $Temps
+@onready var vitesse = $Vitesse
 
-func _process(_delta):
-	label.text = "speed: " + str(snapped(character_body_3d.velocity.length(), 0.01))
+var timeColor : Color
+
+func _process(delta):
+	vitesse.text = "speed: " + str(snapped(character_body_3d.velocity.length(), 0.01))
+	if temps.has_theme_color_override("font_color"):
+		temps.remove_theme_color_override("font_color")
+	if timeColor != Color.WHITE:
+		temps.add_theme_color_override("font_color", timeColor)
+		timeColor = lerp(timeColor, Color.WHITE, 2 * delta)
+	
+
+func time_update(time:float, gotHit:bool):
+	temps.text = str(snapped(time, 0.01))
+	if gotHit:
+		timeColor = Color.RED
+		temps.add_theme_color_override("font_color", timeColor)
