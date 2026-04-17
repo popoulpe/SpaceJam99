f638e5baf4b6a794c6da22cd043d3dbd64337fe5
Dialmee <maggie.fabre02@gmail.com>
2026-04-18 01:31:50 +0200

    [UI][HUD]dialogueScientifique

--- space-jam-99/Scripts/ui_hud_scientifeet.gd
+++ space-jam-99/Scripts/ui_hud_scientifeet.gd
@@ -2,23 +2,23 @@ extends Control
 class_name ui_hud_scientifeet
 
 @onready var character_body_3d = $".."
-@onready var interactPanel = $Control/Panel
-@onready var panel_scientifeet = $"Control/panel du scientifeet"
-@onready var control_scientifeet = $Control
-@onready var label_scientifeet = $"Control/panel du scientifeet/Label"
-
-# nb interaction est supprimable
-#c'est juste du test pour montrer que ca marche
-var nbInteractions :int=0 
+@onready var panel_interact = $interaction
+@onready var panel_scientifeet = $Dialogue
+@onready var all_control = $"."
+@onready var label_scientifeet = $Dialogue/Blabla/RichTextLabel
 
 func show_interact_ui():
-	control_scientifeet.visible = true
+	all_control.visible = true
 
 func hide_interact_ui():
-	control_scientifeet.visible = false
+	all_control.visible = false
 
-func interact() -> void:
-	interactPanel.visible = false
-	panel_scientifeet.visible = true
-	nbInteractions+=1
-	label_scientifeet.text = "interagi " + str(nbInteractions) + " fois"
+func interact(text:String) -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(panel_interact, "scale", Vector2(0.8,0.8), 0.25).set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(panel_interact, "scale", Vector2(1.05,1.05), 0.2).set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(panel_interact, "scale", Vector2(1,1), 0.1).set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_callback(func():
+		panel_interact.visible = false
+		panel_scientifeet.visible = true
+		label_scientifeet.start_dialogue(text))
