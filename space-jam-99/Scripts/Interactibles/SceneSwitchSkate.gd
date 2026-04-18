494b5ae4cb7b373f0ca3f4999bc27d9fae62b0c9
popoulpe <54855619+popoulpe@users.noreply.github.com>
2026-04-18 20:25:35 +0200

    navigation entres scenes

--- space-jam-99/Scripts/Interactibles/SceneSwitchSkate.gd
+++ space-jam-99/Scripts/Interactibles/SceneSwitchSkate.gd
@@ -1,6 +1,7 @@
 extends Interactable
 
 @export var nextLevelScene :String ="res://Scenes/MainScenes/scene_Game_01.tscn"
+@export var isFromEarth :bool= true
 
 func Interact(_player:PlayerWithoutSkate) -> void:
 	get_tree().change_scene_to_file(nextLevelScene)
