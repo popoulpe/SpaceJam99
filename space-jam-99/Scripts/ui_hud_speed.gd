d35e55497568edf25e1cc15fb6e3402989de87e9
Dialmee <maggie.fabre02@gmail.com>
2026-04-19 10:59:51 +0200

    [UI]pauseMenu&&SpeedIsOk

--- space-jam-99/Scripts/ui_hud_speed.gd
+++ space-jam-99/Scripts/ui_hud_speed.gd
@@ -8,7 +8,7 @@ class_name ui_hud_speed
 @onready var vitess_progressBar  = $Vitesse_/TextureProgressBar
 @onready var timeColor_wanted : Color
 var fVitesseDeFou: float = 0.0
-var vectorVitesseTexture:Vector2
+var vectorVitesseText:Vector2
 
 # nb interaction est supprimable
 #c'est juste du test pour montrer que ca marche
@@ -16,11 +16,11 @@ var nbInteractions :int=0
 var timeColor : Color
 
 func _ready()->void:
-	vectorVitesseTexture=vitess_progressBar.position
+	vectorVitesseText=vitesse_big.position
 
 func _process(delta):
 	vitesse_big.text = str(snapped(character_body_3d.velocity.length(), 1))
-	vitesse_little.text = str(snapped(character_body_3d.velocity.length()*1000, 0.0001))
+	vitesse_little.text = str(snapped(character_body_3d.velocity.length()*100, 1))
 	vitess_progressBar.value = snapped(character_body_3d.velocity.length(), 0.1)
 	#if temps.has_theme_color_override("font_color"):
 		#temps.remove_theme_color_override("font_color")
@@ -28,17 +28,22 @@ func _process(delta):
 		temps.add_theme_color_override("font_color", timeColor)
 		timeColor = lerp(timeColor, timeColor_wanted, 2 * delta)
 	
-	fVitesseDeFou += 0.7 * delta
+	fVitesseDeFou += snapped(character_body_3d.velocity.length(), 1) * delta
 	if fVitesseDeFou >= 1.0:
 		fVitesseDeFou = 0.0
 	
 	var angle: float = fVitesseDeFou * TAU
-	var r: float = (sin(angle)*0.5)+0.5 * snapped(character_body_3d.velocity.length(), 1)*10
-	var g:float = (sin(angle + TAU /3)*0.5)+0.5 * snapped(character_body_3d.velocity.length(), 1)*10
-	vitess_progressBar.position = Vector2(vectorVitesseTexture.x+r,vectorVitesseTexture.y+g)
+	var r: float = sin(angle)
+	var g:float = cos(angle)
+	vitesse_big.position = Vector2(vectorVitesseText.x+r,vectorVitesseText.y+g)
 
 func time_update(time:float, gotHit:bool):
-	temps.text = str(snapped(time, 0.01))
+	var snapped_time = snapped(time, 0.01)
+	var seconds = int(snapped_time)
+	var milliseconds = int((snapped_time - seconds) * 100)
+	if (milliseconds < 0):
+		milliseconds = 0
+	temps.text = "%02d : %02d" % [seconds, milliseconds]
 	if gotHit:
 		timeColor = Color.RED
 		temps.add_theme_color_override("font_color", timeColor)
